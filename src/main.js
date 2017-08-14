cc1b8254e2e7a028b489697b78ffcc3c8c7658ba
Dhaval Jardosh <Jardosh@Dhaval-Jardosh.local>
2017-08-14 00:47:57 -0500

    First Commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+export default class Main extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>
+          So now we can render from anywhere....
+          HAhahaha
+        </Text>
+      </View>
+    )
+  }
+};
